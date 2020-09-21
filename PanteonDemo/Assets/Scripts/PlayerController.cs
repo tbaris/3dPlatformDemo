6f75a4a9f923751a779aa068654543bb401c3e46
Barış Türemiş <turemis.baris@gmail.com>
2020-09-21 12:16:52 +0300

    Player Controller added
    Player Controller added

---
+++ PanteonDemo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController CharCont;
+    public float speed;
+    public float turnSmoothTime = 0.05f;
+    float smoothingVel;
+    public Transform cam;
+    public float gravity = 6;
+    public float jumpHeight = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 gravityPull = new Vector3(0f, -gravity * Time.deltaTime, 0f);
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+        Debug.Log(gravityPull);
+        if (Input.GetButtonDown("Jump") /*&& _isGrounded*/)
+        {
+            Debug.Log("jumped"); 
+            gravityPull = new Vector3(0f, Mathf.Sqrt(jumpHeight * -2f * gravity ), 0f);
+            Debug.Log(Mathf.Sqrt(jumpHeight * -2f * gravity));
+        }
+           
+        
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y ;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref smoothingVel, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            
+
+            CharCont.Move((moveDirection.normalized * speed * Time.deltaTime)+ gravityPull);
+        }else
+        {
+            CharCont.Move(gravityPull);
+        }
+        
+    }
+}
---
+++ PanteonDemo/Assets/Scripts/rbPlayerController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rbPlayerController : MonoBehaviour
+{
+   
+    Transform cam;
+
+    public float speed = 10f;    //player speed
+    private float smoothingTurn = 0;    
+    public float turnSmoothTime = 0.1f;  //player rotation smoothing
+    Animator animator;//player animator 
+    Rigidbody rb;//player rigidbody
+
+    Vector3 moveDirection = Vector3.zero;
+    Vector3 camDirection = Vector3.zero;
+
+    public float groundDetectionDistance = 0.1f;
+    public bool isGrounded = false;
+    public bool jumped = false;
+    public float jumpPower = 500;
+    public float jumpReloadTime = 1f;
+
+    void Start()
+    {
+        
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        cam = Camera.main.transform;
+    }
+
+   
+    void Update()
+    {   //rotate player model acording to camera
+        float targetAngle = cam.eulerAngles.y;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref smoothingTurn, turnSmoothTime);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        //move player model acording to camera
+        camDirection = Vector3.Scale(cam.forward, new Vector3(1, 0, 1)).normalized;     
+        moveDirection = Input.GetAxis("Vertical") * camDirection  + Input.GetAxis("Horizontal") * cam.right;
+        moveDirection = moveDirection.normalized * speed;
+
+
+
+
+        if(Input.GetAxis("Jump") >0 && isGrounded && !jumped)
+        {
+            rb.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
+            jumped = true;
+            StartCoroutine("JumpReload");
+        }
+       
+    }
+
+    IEnumerator JumpReload()
+    {
+        
+        yield return new WaitForSeconds(jumpReloadTime);
+        jumped = false;
+    }
+
+    void FixedUpdate()
+    {
+        if(!Physics.Raycast(transform.position,-Vector3.up, groundDetectionDistance ))
+        {
+            isGrounded = false;
+        }
+        else
+        {
+            isGrounded = true;
+            rb.velocity = new Vector3(moveDirection.x, rb.velocity.y, moveDirection.z);
+        }    
+
+    }
+
+}
